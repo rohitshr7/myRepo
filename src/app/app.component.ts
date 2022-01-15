f2bd88dcc12313c51a67bdb56e8e0035791eee9e
rohitshrestha <rohitshresthars7@gmail.com>
2022-01-14 18:36:23 -0600

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {map, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-interview-prep';
+  count = 0
+  public jsonData: any;
+  public jsonDataAll: any;
+  public getJsonURL='https://randomuser.me/api'
+
+  counter(id: any) {
+    console.log(id)
+    id == 1 ? this.count++ : this.count--;
+
+  }
+
+  constructor(public http: HttpClient) {
+    this.getJsonData()
+  }
+
+
+  getJsonData() {
+    this.getData().pipe(map((data) => {
+        this.jsonData = data.results
+         data.results.forEach((test:any)=>{
+           this.jsonDataAll =test
+           JSON.stringify(this.jsonDataAll)
+         })
+        console.log(this.jsonData)
+        console.log(this.jsonDataAll)
+      }
+    )).subscribe()
+
+  }
+
+
+  getData():Observable<any>{
+    return this.http.get(this.getJsonURL)
+
+  }
+
 }
